ae882c2416352ae456fd9755757969f303b3399f
Carlos Guzman Hidalgo <carlosgh89@hotmail.com>
2026-03-23 15:33:01 -0500

    Corregir puerto dinámico para Railway

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
 	"or-halevanah/internal/api"
 )
 
 func main() {
+	// Puerto dinámico para Railway
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Archivos estáticos
 	fs := http.FileServer(http.Dir("./web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// Página principal
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Solo responde index en la raíz exacta
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
 		http.ServeFile(w, r, "./web/templates/index.html")
 	})
 
+	// Registrar rutas API
 	api.RegisterRoutes()
 
-	log.Println("Servidor corriendo en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Servidor corriendo en puerto %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
